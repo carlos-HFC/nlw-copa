c0dd35da8abcc2cb47fc47295701c4af74b78c40
Carlos Faustino <chfcchfc96@gmail.com>
2024-02-16 13:59:42 -0300

    feat: add env validation

---
+++ server/src/env/env.ts
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+import z from "zod";
+
+dotenv.config();
+
+const envSchema = z.object({
+  DATABASE_URL: z.string(),
+  PORT: z.coerce.number().default(3333)
+});
+
+export const env = envSchema.parse(process.env);
